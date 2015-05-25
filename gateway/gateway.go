6889310aed4dabf87bf3668551488b0c1b4bd093
Rui <379630413@qq.com>
2015-05-25 17:30:55 +0800

    服务窗验证通过

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -2,7 +2,7 @@ package gateway
 
 import (
 	"fmt"
-	"github.com/z-ray/alipay/api/sign"
+	signature "github.com/z-ray/alipay/api/sign"
 	"github.com/z-ray/alipaydemo/constants"
 	"github.com/z-ray/alipaydemo/dispatcher"
 	"io/ioutil"
@@ -14,34 +14,75 @@ import (
 // GatewayService 处理支付宝请求
 func GatewayService(w http.ResponseWriter, r *http.Request) {
 
-	body, err := ioutil.ReadAll(r.Body)
+	log.Debug(r.URL.String())
+
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	log.Debugf("receive: %s", bodyBytes)
 	if err != nil {
-		log.Println(err)
+		log.Error(err)
 	}
-	log.Printf("request map params : %s", body)
-	values, _ := url.ParseQuery(string(body))
+	values, err := url.ParseQuery(string(bodyBytes))
+	if err != nil {
+		log.Error(err)
+	}
 
 	// 获取参数
-	service := values.Get("service")
-	signStr := values.Get("sign")
-	signType := values.Get("sign_type")
-	charset := values.Get("charset")
-	content := values.Get("biz_content")
+	params := getParams(values)
 
-	// 按照字典排序
-	data := fmt.Sprintf("biz_content=%s&charset=%s&service=%s&sign_type=%s", content, charset, service, signType)
+	// 排除sign变量
+	sign := params["sign"]
+	delete(params, "sign")
 
-	// 是否需要转编码
-	err = sign.Verify(data, signStr, constants.AliPubKey)
+	// 按照字段排序组装报文
+	body := utils.PrepareContent(params)
+
+	// 验签
+	err = signature.Verfiy(body, sign, aliPubKey)
 	if err != nil {
-		log.Printf("verify wrong %s", err)
-		return
+		log.Errorf("verfiy wrong: %s", err)
 	}
 
-	// 验签成功
-	// 获得执行器
-	action, _ := dispatcher.Executor(values)
-	// 执行
-	result, _ := action.execute()
+	// do something...
+	// TODO router
+	content := "<success>true</success><biz_content>" + cusPubKey + "</biz_content>"
+
+	c, _ := dispatcher.Executor(values)
+
+	// 签名应答
+	signed, err := signature.RsaSign(content, cusPrivKey)
+	if err != nil {
+		log.Error(err)
+	}
+
+	// 组装应答报文
+	respMsg := buildResponse(content, signed)
+	if err != nil {
+		log.Error(err)
+	}
+	log.Debugf("response info : %s", respMsg)
+	w.Write([]byte(respMsg))
+}
+
+// getParams (valuse to map[string]string)
+func getParams(v url.Values) map[string]string {
+
+	params := make(map[string]string)
+	for k, _ := range v {
+		params[k] = v.Get(k)
+	}
+	return params
+}
+
+// buildResponse 构建返回消息体
+func buildResponse(content, signed string) string {
+
+	builder := `<?xml version="1.0" encoding="GBK"?>
+				<alipay>
+					<response>%s</response>
+					<sign>%s</sign>
+					<sign_type>RSA</sign_type>
+				</alipay>`
+
+	return fmt.Sprintf(builder, content, signed)
 
 }
